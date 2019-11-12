06c56fe345d98a4498a424320ec68a36c24aaac2
grok23 <46896171+grok23@users.noreply.github.com>
2019-11-12 14:51:49 +0000

    Add project files.

---
+++ Labsheet7Expenses/Expense.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet7Expenses
+{
+    public class Expense
+    {
+        //declare properties
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+
+        
+        public override string ToString()
+        {
+            return $"{Category}  {Amount:C}  {Date.ToShortDateString()}";
+        }
+
+    }
+}
---
+++ Labsheet7Expenses/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labsheet7Expenses
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //click to add expenses
+        private void btnAddExpenses_Click(object sender, RoutedEventArgs e)
+        {
+            //create expenseitem
+            Expense e1 = new Expense()
+            {
+                Category = "Office",
+                Amount = 15.00m,
+                Date = DateTime.Now
+            };
+
+            //add new expense item
+
+
+
+
+        }
+
+        private void Expenses_Calculator_Loaded(object sender, RoutedEventArgs e) //method runs on window load
+        {
+            //create expense objects
+            Expense e1 = new Expense() { Category = "Travel", Amount = 20.99m, Date = new DateTime(2019, 10, 15) };
+            Expense e2 = new Expense() { Category = "Food", Amount = 9.95m, Date = new DateTime(2019, 10, 5) };
+            Expense e3 = new Expense() { Category = "Office", Amount = 4.80m, Date = new DateTime(2019, 10, 18) };
+            //add them to a list
+            List<Expense> expenses = new List<Expense>();
+            expenses.Add(e1);
+            expenses.Add(e2);
+            expenses.Add(e3);
+            //display the list in the window
+            lbxExpenses.ItemsSource = expenses;
+
+        }
+    }
+}
